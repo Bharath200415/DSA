4f254008028aac046bff76b4bff65b67c26dc4ed
Bharath200415 <bharath200415@gmail.com>
2026-07-01 15:56:45 +0530

    binary serch

--- Arrays/BinarySearch/OrderAgnostic.java
+++ Arrays/BinarySearch/OrderAgnostic.java
@@ -1,28 +1,29 @@
 import java.util.*;
 
-public class BinarySearch{
+public class OrderAgnostic{
     static int search(int[] arr, int target){
-        int start=0;
-        int end = arr.length-1;  
+        int start = 0;
+        int end = arr.length-1;
 
-        while(start<=end){
+        while (start<=end){
             int mid = start+(end-start)/2;
+
             if (arr[mid]==target){
                 return mid;
             }
+
             if (arr[mid]>target){
-                end=mid-1;
-            }else{
                 start=mid+1;
+            }else{
+                end = mid-1;
             }
+            
         }
         return -1;
 
-
     }
-    public static void main (String args[]){
-        int[] arr= {2,12,31,55,63};
-        System.out.println(search(arr,63));
-
+    public static void main(String args[]){
+        int[] arr = {98,86,73,64,43,21};
+        System.out.println(search(arr,86));
     }
 }
